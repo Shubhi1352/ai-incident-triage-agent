26557c767da39bc39e3d9fc2c5bfd5bab919b2b7
Shubhi1352 <103235395+Shubhi1352@users.noreply.github.com>
2026-04-01 05:31:00 +0530

    config changes for deployment

--- ai-incident-triage-frontend/src/service/api.ts
+++ ai-incident-triage-frontend/src/service/api.ts
@@ -1,7 +1,7 @@
 
 
 // src/services/api.ts
-const BASE_URL = 'http://localhost:8080/api';
+const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080/api';
 
 // ✅ Add CORS headers
 const headers = {
